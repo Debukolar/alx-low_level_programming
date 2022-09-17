a96e19d84566ee683d3e254172308898d69e6873
Debukolar <debukolar12@gmail.com>
2022-09-17 08:42:46 -0700

    My task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,20 +12,24 @@ int main(void)
 {
 	int d, p;
 
-	for (d = '0'; d < '9'; d++)
+	for (p = 0; p <= 98; p++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+		for (q = p + 1; q <= 99; q++)
 		{
-			if (p != d)
-			{
-				
-				putchar(d);
-				putchar(p);
-				putchar(',');
-				putchar(' ');
-			}
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+
+			if (p == 98 && q == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
